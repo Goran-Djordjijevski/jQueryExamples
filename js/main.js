9273924dfd76d0189a568a2370dc0b1637106c09
Novisad2 <Novisad2@DESKTOP-JPFB23V>
2017-05-05 09:00:38 +0200

    What is JSON.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
+// Method chaining in jquery
 $("li").css("color", "blue")
        .slideUp(1000)
        .slideDown(1000)
        .attr("title", "My Title");
+
+// What is JSON
+var employeesJSON = [{
+    "firstName": "Goran",
+    "lastName": "Djordjijevski",
+    "gender": "Male",
+    "years": 29
+},
+{
+    "firstName": "Sara",
+    "lastName": "Jovanovic",
+    "gender": "Female",
+    "years": 25 
+}];
+
+var result = "";
+result += "First Name = " + employeesJSON[0].firstName + "<br>";
+result += "Last Name = " + employeesJSON[0].lastName + "<br>";
+result += "Gender = " + employeesJSON[0].gender + "<br>";
+result += "Years = " + employeesJSON[0].years + "<br>";
+
+result += "First Name = " + employeesJSON[1].firstName + "<br>";
+result += "Last Name = " + employeesJSON[1].lastName + "<br>";
+result += "Gender = " + employeesJSON[1].gender + "<br>";
+result += "Years = " + employeesJSON[1].years + "<br>"
+
+$("#result").html(result);
